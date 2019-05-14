b888cf6c5c98f4ec2a209761157236cb4d959c48
smleroy14 <smleroy14@gmail.com>
2019-05-14 23:18:39 +0000

    finalizing create_db.py and creating a blank config file for future app users

--- src/create_db.py
+++ src/create_db.py
@@ -3,18 +3,22 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, MetaData
 import sqlalchemy as sql
 import logging
-import yaml
 import pandas as pd
 import os
-import sys
-
-with open("config.yml", "r") as f:
-    config = yaml.load(f)
+import argparse
 
 # set up looging config
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__file__)
 
+def str2bool(v):
+   if v.lower() in ('yes', 'true', 't', 'y', '1'):
+       return True
+   elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+       return False
+   else:
+       raise argparse.ArgumentTypeError('Boolean value expected.')
+   
 Base = declarative_base()
 
 class Book_Recommendations(Base):
@@ -32,29 +36,61 @@ class Book_Recommendations(Base):
     def __repr__(self):
         return '<Book_Recommendations %r>' % self.Book_Cover
 
-
-def create_db(conn_type, user, password, host, port, DATABASE_NAME):
-    """Creates an RDS database using dbconfig with Book_Recommendations table"""
+def create_db(RDS):
+    """Create a database in RDS or locally on sql lite based on user preference"""
     
-    engine_string = "{}://{}:{}@{}:{}/{}".\
+    if RDS == True:
+        #get configurations from .mysqlconfig
+        conn_type = "mysql+pymysql"
+        user = os.environ.get("MYSQL_USER") 
+        password =  os.environ.get("MYSQL_PASSWORD")
+        host = os.environ.get("MYSQL_HOST") 
+        port = os.environ.get("MYSQL_PORT")
+        DATABASE_NAME = os.environ.get("DATABASE_NAME")
+
+        # the engine_string format
+        #engine_string = "{conn_type}://{user}:{password}@{host}:{port}/DATABASE_NAME"
+        engine_string = "{}://{}:{}@{}:{}/{}".\
+        format(conn_type, user, password, host, port, DATABASE_NAME)
+        print(engine_string)
+        engine = sql.create_engine(engine_string)
+        Base.metadata.create_all(engine)
+    else:
+        SQL_URI = "sqlite:///../data/database.db"
+        engine_string = SQL_URI
+        engine = sql.create_engine(engine_string) 
+        Base.metadata.create_all(engine)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Create database")
+    parser.add_argument('--rds', default=True, type=str2bool, help='True to create RDS database, False creates sqllite database')
+    args = parser.parse_args()
+    create_db(args.rds)
+
+
+def add_to_RDS_db(user_pick, genre, book_rec, book_cover):
+
+    #get configurations from .mysqlconfig
+    conn_type = "mysql+pymysql"
+    user = os.environ.get("MYSQL_USER") 
+    password =  os.environ.get("MYSQL_PASSWORD")
+    host = os.environ.get("MYSQL_HOST") 
+    port = os.environ.get("MYSQL_PORT")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME")
+    
     format(conn_type, user, password, host, port, DATABASE_NAME)
+    # the engine_string format
+    #engine_string = "{conn_type}://{user}:{password}@{host}:{port}/DATABASE_NAME"
+    engine_string = "{}://{}:{}@{}:{}/{}".\
     logger.info(engine_string)
     engine = sql.create_engine(engine_string)
-    Base.metadata.create_all(engine)
-    logger.info("Creating RDS database")
-
-
-def add_to_db(conn_type, user, password, host, port, DATABASE_NAME, user_pick, genre, book_rec, book_cover):
-    
-    engine_string = "{}://{}:{}@{}:{}/{}".\
-    format(conn_type, user, password, host, port, DATABASE_NAME)
-    logger.info(engine_string)
-    engine = sql.create_engine(engine_string)
-    Session = sessionmaker(bind=engine)  
+    Session = sessionmaker(bind=engine)
     session = Session()
     book_rec =  Book_Recommendations(User_Pick = user_pick, Genre= genre, Book_Recommendation= book_rec, Book_Cover = book_cover)
     session.add(book_rec)
     session.commit()
-    logger.info("Added book recommendation to database: " + book_rec)  
+    logger.info("Added book recommendation to database: " + book_rec)
     session.close()
-    
+
+
