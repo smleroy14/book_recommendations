814ef0a6eb167115caa428e3364d4f97feeebf84
smleroy14 <smleroy14@gmail.com>
2019-06-08 22:20:09 -0500

    fixing local sqllite issue

--- flask_config.py
+++ flask_config.py
@@ -10,26 +10,26 @@ APP_NAME = "book-rec"
 #RDS DATABASE
 #--------------
 
-conn_type = "mysql+pymysql"
-user = os.environ.get("MYSQL_USER")
-password = os.environ.get("MYSQL_PASSWORD")
-host = os.environ.get("MYSQL_HOST")
-port = os.environ.get("MYSQL_PORT")
-DATABASE_NAME = 'msia423'
-SQLALCHEMY_DATABASE_URI = "{}://{}:{}@{}:{}/{}".format(conn_type, user, password, host, port, DATABASE_NAME)
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-HOST = "0.0.0.0"
-SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
-MAX_ROWS_SHOW = 100
+#conn_type = "mysql+pymysql"
+#user = os.environ.get("MYSQL_USER")
+#password = os.environ.get("MYSQL_PASSWORD")
+#host = os.environ.get("MYSQL_HOST")
+#port = os.environ.get("MYSQL_PORT")
+#DATABASE_NAME = 'msia423'
+#SQLALCHEMY_DATABASE_URI = "{}://{}:{}@{}:{}/{}".format(conn_type, user, password, host, port, DATABASE_NAME)
+#SQLALCHEMY_TRACK_MODIFICATIONS = True
+#HOST = "0.0.0.0"
+#SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
+#MAX_ROWS_SHOW = 100
 
 #----------------------
 #LOCAL SQLLITE DATABASE
 #----------------------
 
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///data/database.db'
-#SQLALCHEMY_TRACK_MODIFICATIONS = True
-#HOST = "127.0.0.1"
-#SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
-#MAX_ROWS_SHOW = 100
+SQLALCHEMY_DATABASE_URI = 'sqlite:///data/database.db?check_same_thread=False'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+HOST = "127.0.0.1"
+SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
+MAX_ROWS_SHOW = 100
 
 
